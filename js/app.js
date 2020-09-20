e4519d3f90e5c45c9191fdffefafac7657923ebf
Kaito-Dogi <kendobu0405@gmail.com>
2020-09-21 01:34:56 +0900

    particles.js completed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
 particlesJS('particles-js',
+  
   {
     "particles": {
       "number": {
@@ -115,4 +129,5 @@ particlesJS('particles-js',
       "background_size": "cover"
     }
   }
+
 );
